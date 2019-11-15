bdb40b08f0fb8957c78da12c44e5f4405e8afc78
DallinRodgers <rodgers.dallin@gmail.com>
2019-11-15 09:13:15 -0700

    Update main.js
    Created topics array,
    Added eventListeners for the search button,
    Created getNewValue function,
    Created removeBtns function,
    Created createBtns function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,60 @@
+let topics = [
+  "dog",
+  "cat",
+  "fish",
+  "cow",
+  "chicken",
+  "goat",
+  "snake",
+  "bird",
+  "hawk"
+];
+
+const newTopic = document.querySelector("#newBtn");
+const search = document.querySelector("#search");
+const buttons = document.querySelector(".buttons");
+
+search.addEventListener("click", () => {
+  getNewValue();
+});
+
+document.addEventListener("keyup", e => {
+  if (e.keyCode === 13) {
+    getNewValue();
+  }
+});
+
+function getNewValue() {
+  const newValue = newTopic.value;
+  topics.push(newValue);
+  newTopic.value = " ";
+  removeBtns();
+  createBtns(topics);
+}
+
+function removeBtns() {
+  const button = document.querySelectorAll(".button");
+  console.log(button);
+  for (let i = 0; i < button.length; i++) {
+    button[i].remove();
+  }
+}
+
+// function getNewTopic(){
+
+// }
+
+function createBtns(array) {
+  for (let i = 0; i < array.length; i++) {
+    let btn = document.createElement("div");
+    btn.setAttribute("class", "button");
+    btn.innerHTML = array[i];
+    buttons.append(btn);
+  }
+}
+
+createBtns(topics);
+
 // function getGiphy(subject) {
 //   //   This error is prevented when you use https://cors-anywhere.herokuapp.com/
 //   // Access to fetch at has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
@@ -12,9 +69,9 @@
 //     .then(data => {
 //       console.log(data);
 //       // This is a still giph
-//       const giphy = data.data[0].images.original_still.url;
+//         const giphy = data.data[0].images.original_still.url;
 //       // This is the running giph
-//       // const giphy = data.data[0].images.original.url;
+//     //   const giphy = data.data[0].images.original.url;
 //       var oImg = document.createElement("img");
 //       oImg.setAttribute("src", giphy);
 //       oImg.setAttribute("alt", `This is a ${subject} Giphy`);
