0755f6b8c1d1609652f1d43604eb7e72a210ffbb
DallinRodgers <rodgers.dallin@gmail.com>
2019-11-15 10:27:31 -0700

    Update main.js
    Added error handling for newTopic,
    Created addListeners function,
    Started getGiphy

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,13 +13,17 @@ let topics = [
 const newTopic = document.querySelector("#newBtn");
 const search = document.querySelector("#search");
 const buttons = document.querySelector(".buttons");
+const giphs = document.querySelector("#giphs");
+let button = document.querySelectorAll(".button");
 
 search.addEventListener("click", () => {
-  getNewValue();
+  if (newTopic.value !== "") {
+    getNewValue();
+  }
 });
 
 document.addEventListener("keyup", e => {
-  if (e.keyCode === 13) {
+  if (e.keyCode === 13 && newTopic.value !== "" && newTopic.value !== " ") {
     getNewValue();
   }
 });
@@ -33,16 +37,10 @@ function getNewValue() {
 }
 
 function removeBtns() {
-  const button = document.querySelectorAll(".button");
-  console.log(button);
   for (let i = 0; i < button.length; i++) {
     button[i].remove();
   }
 }
-
-// function getNewTopic(){
-
-// }
 
 function createBtns(array) {
   for (let i = 0; i < array.length; i++) {
@@ -51,36 +49,43 @@ function createBtns(array) {
     btn.innerHTML = array[i];
     buttons.append(btn);
   }
+  button = document.querySelectorAll(".button");
+  addListeners(button);
+}
+
+function addListeners(array) {
+  for (let i = 0; i < array.length; i++) {
+    array[i].addEventListener("click", function() {
+      //   clearGiphs();
+      getGiphy(topics[i]);
+    });
+  }
 }
 
 createBtns(topics);
 
-// function getGiphy(subject) {
-//   //   This error is prevented when you use https://cors-anywhere.herokuapp.com/
-//   // Access to fetch at has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
-//   fetch(
-//     `https://cors-anywhere.herokuapp.com/https://api.giphy.com/v1/gifs/search?api_key=OLXBqiRTp41fGqAd42KwrJtbhuKQIhYJ&q=${subject}&limit=1&offset=0&rating=G&lang=en`
-//   )
-//     .then(result => {
-//       // body: ReadableStream, this is found in the returned results from the api
-//       // This will return a promise and convert it from json to javascript
-//       return result.json();
-//     })
-//     .then(data => {
-//       console.log(data);
-//       // This is a still giph
-//         const giphy = data.data[0].images.original_still.url;
-//       // This is the running giph
-//     //   const giphy = data.data[0].images.original.url;
-//       var oImg = document.createElement("img");
-//       oImg.setAttribute("src", giphy);
-//       oImg.setAttribute("alt", `This is a ${subject} Giphy`);
-//       document.body.appendChild(oImg);
-//       console.log(giphy);
-//     })
-//     .catch(error => console.log(error));
-// }
-
-// getGiphy("dog");
-// getGiphy("cat");
-// getGiphy("funny");
+function getGiphy(subject) {
+  //   This error is prevented when you use https://cors-anywhere.herokuapp.com/
+  // Access to fetch at has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+  fetch(
+    `https://cors-anywhere.herokuapp.com/https://api.giphy.com/v1/gifs/search?api_key=OLXBqiRTp41fGqAd42KwrJtbhuKQIhYJ&q=${subject}&limit=1&offset=0&rating=G&lang=en`
+  )
+    .then(result => {
+      // body: ReadableStream, this is found in the returned results from the api
+      // This will return a promise and convert it from json to javascript
+      return result.json();
+    })
+    .then(data => {
+      console.log(data);
+      // This is a still giph
+      const giphy = data.data[0].images.original_still.url;
+      // This is the running giph
+      //   const giphy = data.data[0].images.original.url;
+      var oImg = document.createElement("img");
+      oImg.setAttribute("src", giphy);
+      oImg.setAttribute("alt", `This is a ${subject} Giphy`);
+      giphs.appendChild(oImg);
+      //   console.log(giphy);
+    })
+    .catch(error => console.log(error));
+}
