1180671e64850552b7c299c37f1d60b2cb022ac8
DallinToastMEN <rodgers.dallin@gmail.com>
2019-11-14 19:46:47 -0700

    Starter Files
    Created starter files and folders,
    Added getGiphy function to main.js

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+// function getGiphy(subject) {
+//   //   This error is prevented when you use https://cors-anywhere.herokuapp.com/
+//   // Access to fetch at has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+//   fetch(
+//     `https://cors-anywhere.herokuapp.com/https://api.giphy.com/v1/gifs/search?api_key=OLXBqiRTp41fGqAd42KwrJtbhuKQIhYJ&q=${subject}&limit=1&offset=0&rating=G&lang=en`
+//   )
+//     .then(result => {
+//       // body: ReadableStream, this is found in the returned results from the api
+//       // This will return a promise and convert it from json to javascript
+//       return result.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//       // This is a still giph
+//       const giphy = data.data[0].images.original_still.url;
+//       // This is the running giph
+//       // const giphy = data.data[0].images.original.url;
+//       var oImg = document.createElement("img");
+//       oImg.setAttribute("src", giphy);
+//       oImg.setAttribute("alt", `This is a ${subject} Giphy`);
+//       document.body.appendChild(oImg);
+//       console.log(giphy);
+//     })
+//     .catch(error => console.log(error));
+// }
+
+// getGiphy("dog");
+// getGiphy("cat");
+// getGiphy("funny");
