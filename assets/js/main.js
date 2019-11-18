3288efa27e6b111185375e15e94b04923287c668
DallinToastMEN <rodgers.dallin@gmail.com>
2019-11-18 07:44:59 -0700

    Update main.js
    Added comments for each funtion,
    Working on creating eventListeners for giphs

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,53 +16,51 @@ const buttons = document.querySelector(".buttons");
 const giphs = document.querySelector("#giphs");
 let button = document.querySelectorAll(".button");
 
+// Event listener for clicking search button
 search.addEventListener("click", () => {
-  if (newTopic.value !== "") {
+  if (newTopic.value !== "" && newTopic.value !== " ") {
     getNewValue();
   }
 });
 
+// Event listener for enter key
 document.addEventListener("keyup", e => {
   if (e.keyCode === 13 && newTopic.value !== "" && newTopic.value !== " ") {
     getNewValue();
   }
 });
 
+// Get value from input and add it to topics array
 function getNewValue() {
   const newValue = newTopic.value;
   topics.push(newValue);
-  newTopic.value = " ";
+  newTopic.value = "";
   removeBtns();
   createBtns(topics);
 }
 
+// Remove all buttons
 function removeBtns() {
   for (let i = 0; i < button.length; i++) {
     button[i].remove();
   }
 }
 
+// Create new buttons for each item in the topics array
 function createBtns(array) {
   for (let i = 0; i < array.length; i++) {
-    let btn = document.createElement("div");
+    let btn = document.createElement("a");
     btn.setAttribute("class", "button");
     btn.innerHTML = array[i];
     buttons.append(btn);
   }
   button = document.querySelectorAll(".button");
-  addListeners(button);
+  addListeners(button, "btn");
 }
 
-function addListeners(array) {
-  for (let i = 0; i < array.length; i++) {
-    array[i].addEventListener("click", function() {
-      clearGiphs();
-      getGiphy(topics[i]);
-    });
-  }
-}
+// Remove all giphs
 function clearGiphs() {
-  const clearArray = document.querySelectorAll(".giph");
+  const clearArray = document.querySelectorAll(".giphDiv");
   for (let i = 0; i < clearArray.length; i++) {
     clearArray[i].remove();
   }
@@ -70,6 +68,7 @@ function clearGiphs() {
 
 createBtns(topics);
 
+// Get new giphs from GIPHY API for the given topic
 function getGiphy(subject) {
   //   This error is prevented when you use https://cors-anywhere.herokuapp.com/
   // Access to fetch at has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
@@ -82,19 +81,52 @@ function getGiphy(subject) {
       return result.json();
     })
     .then(data => {
-      console.log(data);
+      const giphsArray = data.data;
       for (let i = 0; i < data.data.length; i++) {
         // This is a still giph
-        const giphy = data.data[i].images.original_still.url;
+        const rating = giphsArray[i].rating;
+        // const giphy = giphsArray[i].images.original_still.url;
         // This is the running giph
-        // const giphy = data.data[i].images.original.url;
+        const giphy = data.data[i].images.original.url;
         var oImg = document.createElement("img");
         oImg.setAttribute("src", giphy);
         oImg.setAttribute("alt", `This is a ${subject} Giphy`);
-        oImg.setAttribute("class", "giph");
-        giphs.appendChild(oImg);
-        //   console.log(giphy);
+        oImg.setAttribute("class", "giph still");
+        // oImg.innerHTML = `<img src=${giphy} alt="This is a ${subject} Giphy" class="giph still"`;
+        const giphDIV = document.createElement("div");
+        giphDIV.setAttribute("class", "giphDiv");
+        giphDIV.innerHTML = `<p class='rating'>Rating: ${rating}<p>`;
+        // giphs.appendChild(oImg);
+        giphs.appendChild(giphDIV);
+        giphDIV.appendChild(oImg);
       }
+      addListeners(giphsArray, "giph");
     })
     .catch(error => console.log(error));
+}
+
+// Add event listeners for each new button
+function addListeners(array, item) {
+  if (item === "btn") {
+    for (let i = 0; i < array.length; i++) {
+      array[i].addEventListener("click", function() {
+        clearGiphs();
+        getGiphy(topics[i]);
+      });
+    }
+  } else if (item === "giph") {
+    console.log(array);
+    // add event listeners to giphs
+    // for (let i = 0; i < giphArray.length; i++) {
+    // check class list
+    // If still remove still and add playing
+    // update src to correct url
+    // this.setAttribute("src", giphy);
+    // }
+    for (let i = 0; i < array.length; i++) {
+      array[i].addEventListener("click", function() {
+        console.log("you clicked a giph");
+      });
+    }
+  }
 }
